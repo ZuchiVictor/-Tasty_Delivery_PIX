155adc3c237fccf0b9296eb81bf0e84e3d5418df
Zucchi <zuchi25@gmail.com>
2024-05-23 23:16:07 -0300

    adicionado teste unitario

--- SRC/PixMicroservice/DAO/PixTransaction.cs
+++ SRC/PixMicroservice/DAO/PixTransaction.cs
@@ -1,10 +1,31 @@
-﻿namespace PixMicroservice.DAO
+﻿using Couchbase;
+using System.Text.Json.Serialization;
+
+namespace PixMicroservice.DAO
 {
     public class PixTransaction
     {
+
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+        [JsonPropertyName("pixKey")]
         public string PixKey { get; set; }
+        [JsonPropertyName("amount")]
         public decimal Amount { get; set; }
+        [JsonPropertyName("transactionDate")]
         public DateTime TransactionDate { get; set; }
+        public string name { get; set; }
+
+        public string sobrenome { get; set; }
+
+        public string  cpf { get; set; }
+        public string Email { get; set; }
     }
+
+
+    public class PixBucketContainer
+    {
+        public PixTransaction PixBucket { get; set; }
+    }
+
 }
---
+++ SRC/PixMicroservice/MercadoPago/IML.cs
@@ -0,0 +1,10 @@
+﻿using MercadoPago.Resource.Payment;
+using PixMicroservice.DAO;
+
+namespace PixMicroservice.MercadoPago
+{
+    public interface IML
+    {
+        Payment Pix(PixTransaction pix);
+    }
+}
